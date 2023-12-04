adc3f9722bc04f4545962fae458c6c9ba0f25e0e
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-12-04 23:13:26 +0200

    Tested controller methods request actions

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :request do
+  context 'index action method' do
+    it 'should return correct response status' do
+      get '/users/1/posts'
+      expect(response).to(have_http_status(:success))
+    end
+    it 'should render correct view template' do
+      get '/users/1/posts'
+      expect(response).to(render_template(:index))
+    end
+    it 'should return correct response status' do
+      get '/users/1/posts'
+      expect(response.body).to(include('Posts list'))
+    end
+  end
+  context 'show action method' do
+    it 'should return correct response status' do
+      get '/users/1/posts/1'
+      expect(response).to(have_http_status(:success))
+    end
+    it 'should render correct view template' do
+      get '/users/1/posts/1'
+      expect(response).to(render_template(:show))
+    end
+    it 'should return correct response status' do
+      get '/users/1/posts/1'
+      expect(response.body).to(include('Post page'))
+    end
+  end
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :request do
+  context 'index action method' do
+    it 'should return correct response status' do
+      get '/users/'
+      expect(response).to(have_http_status(:success))
+    end
+    it 'should render correct view template' do
+      get '/users/'
+      expect(response).to(render_template(:index))
+    end
+    it 'should return correct response status' do
+      get '/users/'
+      expect(response.body).to(include('Users list'))
+    end
+  end
+  context 'show action method' do
+    it 'should return correct response status' do
+      get '/users/1'
+      expect(response).to(have_http_status(:success))
+    end
+    it 'should render correct view template' do
+      get '/users/1'
+      expect(response).to(render_template(:show))
+    end
+    it 'should return correct response status' do
+      get '/users/1'
+      expect(response.body).to(include('User page'))
+    end
+  end
+end
