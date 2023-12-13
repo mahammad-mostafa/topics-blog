a28968e376dd03a3f51d48534ea7c1bc31c23c14
Zabih Noori <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-12-13 20:20:01 +0430

    Create users_spec.rb with index action tests

---
+++ spec/systems/users_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :system do
+  before do
+    @user = User.find(1)
+  end
+  describe 'index action page' do
+    it 'should display name of each user' do
+      visit('/users')
+      expect(page).to(have_content(@user.name))
+    end
+    it 'should display photo of each user' do
+      visit('/users')
+      expect(first('.user').find('img')['src']).to(eq(@user.photo))
+    end
+    it 'should display number of posts for each user' do
+      visit('/users')
+      expect(first('.user')).to(have_content("Posts: #{@user.posts_counter}"))
+    end
+    it 'should open each user show action page with a link' do
+      visit('/users')
+      first('.user').click_link('Details')
+      expect(page).to(have_current_path(user_path(@user)))
+    end
+  end
+end
