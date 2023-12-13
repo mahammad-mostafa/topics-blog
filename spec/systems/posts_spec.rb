730b13676d7758e332f0bdad1e9f62d77af0c9b4
Zabih Noori <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-12-13 20:23:57 +0430

    Update posts_spec.rb with show action tests

--- spec/systems/posts_spec.rb
+++ spec/systems/posts_spec.rb
@@ -52,4 +52,39 @@ RSpec.describe PostsController, type: :system do
       expect(page).to(have_current_path(user_post_path(@post.user, @post)))
     end
   end
+  describe 'show action page' do
+    it 'should display name of the post user' do
+      visit('/users/1/posts/1')
+      expect(page).to(have_content(@post.user.name))
+    end
+    it 'should display title of the post' do
+      visit('/users/1/posts/1')
+      expect(page).to(have_content(@post.title))
+    end
+    it 'should display body of the post' do
+      visit('/users/1/posts/1')
+      expect(page).to(have_content(@post.text))
+    end
+    it 'should display number of comments for the post' do
+      visit('/users/1/posts')
+      expect(page).to(have_content("Comments: #{@post.comments_counter}"))
+    end
+    it 'should display number of likes for the post' do
+      visit('/users/1/posts')
+      expect(page).to(have_content("Likes: #{@post.likes_counter}"))
+    end
+    it 'should display names of commentors for each comment' do
+      visit('/users/1/posts/1')
+      expect(page).to(have_content(@post.comments.first.user.name))
+    end
+    it 'should display text of each comment' do
+      visit('/users/1/posts/1')
+      expect(page).to(have_content(@post.comments.first.text))
+    end
+    it 'should open the user posts page with a link' do
+      visit('/users/1/posts/1')
+      click_link('Back')
+      expect(page).to(have_current_path(user_posts_path(@post.user)))
+    end
+  end
 end
