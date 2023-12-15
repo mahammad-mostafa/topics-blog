53535458efad59ec5262fbac6b2daa9055819eb9
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-12-15 17:46:49 +0200

    Refactored all tests to use setup_helper module

--- spec/systems/users_show_spec.rb
+++ spec/systems/users_show_spec.rb
@@ -1,49 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :system do
-  before do
-    @user = User.create(id: 1, name: 'name', photo: 'https://placehold.co/100', bio: 'bio', posts_counter: 0)
-    Post.create(id: 1, title: 'title', text: 'text', comments_counter: 0, likes_counter: 0, user: @user)
+  let!(:user) do
+    initializer
+    user = User.first
+    sign_in(user)
+    user.confirm
+    user
   end
+  let!(:path) { user_path(user) }
   describe 'show action page' do
     it 'should display name of the user' do
-      visit('/users/1')
-      expect(page).to(have_content(@user.name))
+      visit(path)
+      expect(page).to(have_content(user.name))
     end
     it 'should display photo of the user' do
-      visit('/users/1')
-      expect(find('.user img')['src']).to(eq(@user.photo))
+      visit(path)
+      expect(find('.user img')['src']).to(eq(user.photo))
     end
     it 'should display number of posts for the user' do
-      visit('/users/1')
-      expect(page).to(have_content("Posts: #{@user.posts_counter}"))
+      visit(path)
+      expect(page).to(have_content("Posts: #{user.posts_counter}"))
     end
     it 'should display bio of the user' do
-      visit('/users/1')
-      expect(page).to(have_content(@user.bio))
+      visit(path)
+      expect(page).to(have_content(user.bio))
     end
     it 'should display three posts at most for the user' do
-      visit('/users/1')
+      visit(path)
       expect(all('.user').length).to(be_between(0, 3).inclusive)
     end
     it 'should have a link to open all posts of the user' do
-      visit('/users/1')
+      visit(path)
       expect(page).to(have_content('All posts'))
     end
     it 'should open all users page with a link' do
-      visit('/users/1')
+      visit(path)
       click_link('Back')
       expect(page).to(have_current_path(users_path))
     end
     it 'should open each post show page with a link' do
-      visit('/users/1')
+      visit(path)
       find('.post').click_link('Details')
-      expect(page).to(have_current_path(user_post_path(@user, @user.posts.first)))
+      expect(page).to(have_current_path(user_post_path(user, user.posts.first)))
     end
     it 'should open all user posts with a link' do
-      visit('/users/1')
+      visit(path)
       click_link('All posts')
-      expect(page).to(have_current_path(user_posts_path(@user)))
+      expect(page).to(have_current_path(user_posts_path(user)))
     end
   end
 end
