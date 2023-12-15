682b2eb9236eac57a0cf6dd5202534ad373cb1a4
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-12-15 17:20:33 +0200

    Appended extra attributes with sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @user = User.find(1)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio posts_counter])
   end
 end
