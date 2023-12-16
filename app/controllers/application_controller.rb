80462394f5807a1b5bb0c4f24ed744e7a0225f89
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-12-16 21:40:11 +0200

    Handled invalid paths or records along with invalid access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound do
+    redirect_to(root_url, alert: 'Record not found')
+  end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to(root_url, alert: exception.message)
+  end
+
+  def index
+    redirect_to(root_url, alert: 'Invalid path')
+  end
+
   protected
 
   def configure_permitted_parameters
